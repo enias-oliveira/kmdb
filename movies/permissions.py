2c0563453f146d2a32bf19d92ac4c52b072f9c50
Enias Oliveira <eniasoliveira27@gmail.com>
2021-05-21 18:32:16 -0300

    fix: adds movie listing for anonymous users

--- movies/permissions.py
+++ movies/permissions.py
@@ -1,11 +1,14 @@
 from rest_framework import permissions
 
 
-class IsAdmin(permissions.BasePermission):
+class IsAdminOrAnonReadOnly(permissions.BasePermission):
     message = "you do not have permission to perform this action."
 
     def has_permission(self, request, view):
         if request.user.is_superuser:
+            return True
+
+        if request.method in permissions.SAFE_METHODS:
             return True
 
         return False
--- movies/views.py
+++ movies/views.py
@@ -1,15 +1,16 @@
 from rest_framework import viewsets
+
 from rest_framework.authentication import TokenAuthentication
 
 from .models import Movie
 
 from .serializers import MovieSerializer
 
-from .permissions import IsAdmin
+from .permissions import IsAdminOrAnonReadOnly
 
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAdmin]
+    permission_classes = [IsAdminOrAnonReadOnly]
